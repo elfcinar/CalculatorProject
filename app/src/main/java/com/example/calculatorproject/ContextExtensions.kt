51ea457e19093dd49438ed99ea0c134371ac0dc4
Elif Çınar <elif.cinar@lecten.com>
2023-08-23 00:12:47 +0300

    timer added

--- app/src/main/java/com/example/calculatorproject/ContextExtensions.kt
+++ app/src/main/java/com/example/calculatorproject/ContextExtensions.kt
@@ -3,6 +3,7 @@ package com.example.calculatorproject
 import android.content.Context
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 
 fun Context.showToast(message:String){
@@ -11,6 +12,10 @@ fun Context.showToast(message:String){
 
 fun Context.showToast(message:Int){
     Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.showAlert(message:String){
+    AlertDialog.Builder(this).setMessage(message).create().show()
 }
 
 fun Context.showSnackBar(view: View, message:String){
